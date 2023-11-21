4d9e756176128f3e3ac351eebe01535a2cb14d0e
laribrito <lary.29.ds@gmail.com>
2023-11-21 10:53:13 -0300

    fix: fixes Label component

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,13 +1,20 @@
-import React, { HtmlHTMLAttributes } from "react"
+import React, { HTMLProps } from "react"
 import styles from "./label.module.css"
 
-interface labelProps {
+interface labelProps extends HTMLProps<HTMLLabelElement> {
     tipo: "titulo1" | "titulo2" | "titulo3" | "label" | "texto" | "label discreto"
     valor: string
 } 
 
-export default function Label({tipo, valor}: labelProps){
-    function labelConstr(valor: string, styleClass: string){
+export default function Label({tipo, valor, ...rest}: labelProps){
+    function labelConstr(styleClass: string){7
+        if (styleClass == styles.label){
+            return (
+                <label className={styleClass} {...rest}>
+                    {valor}
+                </label>
+            )
+        }
         return (
             <p className={styleClass}>
                 {valor}
@@ -17,12 +24,12 @@ export default function Label({tipo, valor}: labelProps){
 
     return (
         <>
-            {tipo=="titulo1" && labelConstr(valor, styles.titulo1)}
-            {tipo=="titulo2" && labelConstr(valor, styles.titulo2)}
-            {tipo=="titulo3" && labelConstr(valor, styles.titulo3)}
-            {tipo=="label" && labelConstr(valor, styles.label)}
-            {tipo=="texto" && labelConstr(valor, styles.texto)}
-            {tipo=="label discreto" && labelConstr(valor, styles.labelDiscreto)}
+            {tipo=="titulo1" && labelConstr(styles.titulo1)}
+            {tipo=="titulo2" && labelConstr(styles.titulo2)}
+            {tipo=="titulo3" && labelConstr(styles.titulo3)}
+            {tipo=="label" && labelConstr(styles.label)}
+            {tipo=="texto" && labelConstr(styles.texto)}
+            {tipo=="label discreto" && labelConstr(styles.labelDiscreto)}
         </>
     )
 }
