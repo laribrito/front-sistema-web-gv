1b85b108988f2fbf3387dbc2c1eb39df3f76305b
laribrito <lary.29.ds@gmail.com>
2023-11-23 11:56:48 -0300

    refactor: refactor the InputText component

--- src/components/Input/InputText/index.tsx
+++ src/components/Input/InputText/index.tsx
@@ -1,16 +1,35 @@
 import React, {InputHTMLAttributes} from "react";
 import stylesInput from "../input.module.css"
 import { InputGeneric } from "../interfaceInput";
+import { TextField, ThemeProvider, createTheme } from "@mui/material";
 
 interface InputTextProps extends InputGeneric, InputHTMLAttributes<HTMLInputElement> {
   type: "text" | "email"
 }
 
 export default function InputText({type, id, label, ...rest}:InputTextProps) {
+  const theme = createTheme({
+    components: {
+      // Name of the component
+      MuiTextField: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            width: '100%',
+            background: 'white'
+          },
+        },
+      },
+    },
+  });
+  
   return (
     <div className={stylesInput.formField}>
       <label htmlFor={id}>{`${label}:`}</label>
-      <input type={type} className={stylesInput.box} {...rest} id={id}/>
+      <ThemeProvider theme={theme}>
+        <TextField id="outlined-basic" variant="outlined" size="small"/>
+      </ThemeProvider>
     </div>
   );
 }
