f6a4d012e836f87c5deb1770761cc197aca11042
laribrito <lary.29.ds@gmail.com>
2023-12-20 08:57:05 -0300

    fix: removes warnings in component InputSelect

--- src/components/Input/InputSelect/index.tsx
+++ src/components/Input/InputSelect/index.tsx
@@ -1,19 +1,32 @@
-import React, {SelectHTMLAttributes} from "react";
+import {SelectHTMLAttributes, useState, ChangeEvent} from "react";
 import stylesInput from "../input.module.css"
 import { InputGeneric, Option, themeMUI, variantOfInputs } from "../interfaceInput";
-import { MenuItem, Select, ThemeProvider } from "@mui/material";
+import { MenuItem, Select, SelectChangeEvent, ThemeProvider } from "@mui/material";
 
 interface InputSelectProps extends InputGeneric, SelectHTMLAttributes<HTMLSelectElement>{
   options: Option[] | null
 }
 
 export default function InputSelect({options, id, label, name, ...rest}:InputSelectProps) {
+  const [selectedValue, setSelectedValue] = useState<HTMLSelectElement | undefined>((options)? options[0].id as unknown as HTMLSelectElement: undefined);
+
+  const handleSelectChange = (event: SelectChangeEvent<HTMLSelectElement>) => {
+    setSelectedValue(event.target.value as HTMLSelectElement)
+  };
+  
   return (
     <div className={stylesInput.formField}>
       <label htmlFor={id}>{`${label}:`}</label>
 
       <ThemeProvider theme={themeMUI}>
-        <Select variant={variantOfInputs} size="small" defaultValue={1} hiddenLabel name={name} {...rest}>
+        <Select 
+          variant={variantOfInputs} 
+          size="small" 
+          value={selectedValue || ""}
+          onChange={handleSelectChange}
+          hiddenLabel 
+          name={name} 
+        >
           {options && options.map((op) => (
             <MenuItem key={op.id} value={op.id}>
               {op.valor}
